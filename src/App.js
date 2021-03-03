ac6bcaeb9aa3a21b9ada0d6a897c697625c1e66b
dannyr22 <dannyr2763@hotmail.com>
2021-03-03 08:14:18 +0000

    initial commit of goal manager

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import Airtable from 'airtable';
+import { useEffect, useState } from 'react'
+import Goal from './components/Goal';
+import styled from 'styled-components';
 
+import { GlobalStyle } from './styles/Global.style';
+
+const StyledH1 = styled.h1` 
+text-align: ceneter;
+font-size: 4rem;
+margin: 1rem 0rem;
+`
+
+const base = new Airtable({ apiKey: "keyZECWHt2mmot3G7" }).base("appmBas8uUiznpCO5");
 function App() {
+  const [updates, setUpdates] = useState([])
+  const [goals, setGoals] = useState([])
+  useEffect(() => {
+    base("goals")
+      .select({ view: "Grid view" })
+      .eachPage(( records, fetchNextPage ) => {
+      setGoals(records);
+      fetchNextPage();
+      })
+    base("updates")
+      .select({ view: "Grid view" })
+      .eachPage(( records, fetchNextPage ) => {
+      setUpdates(records);
+      fetchNextPage();
+      })
+  }, [])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <StyledH1>My Goals</StyledH1>
+      {goals.map(goal => (
+        <Goal
+          key={goal.id}
+          goal={goal}
+        updates={updates.filter(update => update.fields.goalid === goal.id)}/>
+      
+      ))}
+    </>
+    
   );
 }
 
